3f239c199ad0edfb9d1b66c1b7fc3d603a1a9c0a
fayezmohammad096 <fayezmohammad096@gmail.com>
2024-02-21 12:54:53 +0600

    Registration functionality added

--- config/user/views.py
+++ config/user/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
-
+from . forms import CustomUserCreationForm
 # Create your views here.
 
 def loginuser(request):
@@ -31,4 +31,19 @@ def logoutUser(request):
      logout(request)
      messages.success(request,'Successfully Logout')
      return redirect('login')
+
+
+
+def signupView(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Registration Successfull")
+            return redirect('login')
+        else:
+            messages.error(request,"Form Invalid or empty")
+    else:
+        form = CustomUserCreationForm()
+    return render(request,'user/signup.html',{'form':form})
 
