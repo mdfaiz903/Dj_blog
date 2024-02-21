90ba10b9b6ec9508a6e08f20cd0ca7bf60f9782a
fayezmohammad096 <fayezmohammad096@gmail.com>
2024-02-21 12:03:53 +0600

    login functionality added using crispy bootstrap_5

--- config/config/settings.py
+++ config/config/settings.py
@@ -39,7 +39,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'post',
     'user',
+
+    'crispy_forms',
+    "crispy_bootstrap5",
 ]
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
