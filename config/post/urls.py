a453d77df6fa369964bc58c51dc702addfa78e79
fayezmohammad096 <fayezmohammad096@gmail.com>
2024-02-27 21:49:01 +0600

    post create edit part1

--- config/post/urls.py
+++ config/post/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from.views import homeView,PostDetails
+from.views import homeView,PostDetails,PostCreate
 urlpatterns = [
     path('',homeView.as_view(),name='home'),
     path('postdetails/<int:pk>',PostDetails.as_view(),name='postdetails'),
+    path('post-create/',PostCreate.as_view(),name='post-create'),
 ]
--- config/post/views.py
+++ config/post/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.views import generic
 from . models import post_model
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
+class PostCreate(LoginRequiredMixin,generic.CreateView):
+    model = post_model
+    template_name = 'post/postCreate.html'
+    fields = ['title','description']
+    success_url = '/'
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
 class PostDetails(generic.DetailView):
     model = post_model
     template_name = 'post/details.html'
