206cbb4f262201499510881f25dd9e9972d98301
fayezmohammad096 <fayezmohammad096@gmail.com>
2024-02-28 20:54:42 +0600

    post update & delete class functionality added

--- config/post/urls.py
+++ config/post/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from.views import homeView,PostDetails,PostCreate
+from.views import homeView,PostDetails,PostCreate,PostUpdateView,post_delete
 urlpatterns = [
+
     path('',homeView.as_view(),name='home'),
     path('postdetails/<int:pk>',PostDetails.as_view(),name='postdetails'),
     path('post-create/',PostCreate.as_view(),name='post-create'),
+    path('post/<int:pk>/Delete/',post_delete.as_view(),name='post_delete'),
+    path('post/<int:pk>/update/',PostUpdateView.as_view(),name='post_update'),
+  
 ]
--- config/post/views.py
+++ config/post/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
 from django.views import generic
 from . models import post_model
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.urls import reverse_lazy
+
 # Create your views here.
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,generic.UpdateView):
+    model = post_model
+    template_name = 'post/post_update.html'
+    fields = ['title','description']
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    def test_func(self):
+        post  = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+    success_url ='/'
+class post_delete(LoginRequiredMixin,UserPassesTestMixin, generic.DeleteView):
+    model = post_model
+    template_name = 'post/delete.html'
+    def test_func(self):
+        post  = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+    success_url ='/'
 
 class PostCreate(LoginRequiredMixin,generic.CreateView):
     model = post_model
