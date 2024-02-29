3b0fd5282da7c6fd345d0a9935ce2be3352b99bc
fayezmohammad096 <fayezmohammad096@gmail.com>
2024-02-29 21:05:01 +0600

    Customize admin panel

--- config/post/admin.py
+++ config/post/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from . models import post_model
 # Register your models here.
 
-admin.site.register(post_model)
+admin.site.site_header = "Django Blog website"
+admin.site.site_title = "Django Blog website"
+admin.site.index_title = ''
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('author','title','created_date',)
+    list_filter = ('author','title','created_date',)
+
+admin.site.register(post_model,PostAdmin)
